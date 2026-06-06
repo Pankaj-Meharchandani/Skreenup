29af35ee00215e144834b5ade4c7b544671f2dac
Pankaj Meharchandani <pmeharchandani@yahoo.com>
2026-06-07 03:42:13 +0530

    Updated to v1.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.skreenup"
         minSdk = 30
         targetSdk = 37
-        versionCode = 6
-        versionName = "1.5.0"
+        versionCode = 7
+        versionName = "1.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
